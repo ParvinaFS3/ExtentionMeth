587bd6e94139020c67498bdfa71bc5399c967cee
ParvinaFS3 <parvinaisma@gmail.com>
2023-11-15 00:24:01 +0400

    first commit

---
+++ task-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class ArrayExtensions
+{
+    public static int[] Add(this int[] array, int element)
+    {
+        int[] newArray = new int[array.Length + 1];
+        Array.Copy(array, newArray, array.Length);
+        newArray[array.Length] = element;
+        return newArray;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[0];
+        arr = arr.Add(1);
+        arr = arr.Add(2);
+        arr = arr.Add(3);
+        arr = arr.Add(4);
+
+        int sum = 0;
+        foreach (var item in arr)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine(sum); 
+    }
+}
